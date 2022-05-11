defddcaf7b3e9912216f2b45aabc14cc01c97c30
longlivefloppa <peterbedrosasdourian@gmail.com>
2022-05-12 00:51:49 +0300

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,95 @@
+document.getElementById("1").addEventListener("click", playAudio);
+function playAudio() {
+    let x = document.getElementById("audio1");
+    x.play();
+}
+
+document.getElementById("2").addEventListener("click", playAudio2);
+function playAudio2() {
+    let x = document.getElementById("audio2");
+    x.play();
+}
+
+document.getElementById("3").addEventListener("click", playAudio3);
+function playAudio3() {
+    let x = document.getElementById("audio3");
+    x.play();
+}
+
+document.getElementById("4").addEventListener("click", playAudio4);
+function playAudio4() {
+    let x = document.getElementById("audio4");
+    x.play();
+}
+
+document.getElementById("5").addEventListener("click", playAudio5);
+function playAudio5() {
+    let x = document.getElementById("audio5");
+    x.play();
+}
+
+document.getElementById("6").addEventListener("click", playAudio6);
+function playAudio6() {
+    let x = document.getElementById("audio6");
+    x.play();
+}
+
+document.getElementById("7").addEventListener("click", playAudio7);
+function playAudio7() {
+    let x = document.getElementById("audio7");
+    x.play();
+}
+
+document.getElementById("8").addEventListener("click", playAudio8);
+function playAudio8() {
+    let x = document.getElementById("audio8");
+    x.play();
+}
+
+document.getElementById("9").addEventListener("click", playAudio9);
+function playAudio9() {
+    let x = document.getElementById("audio9");
+    x.play();
+}
+
+document.getElementById("10").addEventListener("click", playAudio10);
+function playAudio10() {
+    let x = document.getElementById("audio10");
+    x.play();
+}
+
+document.getElementById("11").addEventListener("click", playAudio11);
+function playAudio11() {
+    let x = document.getElementById("audio11");
+    x.play();
+}
+
+document.getElementById("12").addEventListener("click", playAudio12);
+function playAudio12() {
+    let x = document.getElementById("audio12");
+    x.play();
+}
+
+document.getElementById("13").addEventListener("click", playAudio13);
+function playAudio13() {
+    let x = document.getElementById("audio13");
+    x.play();
+}
+
+document.getElementById("14").addEventListener("click", playAudio14);
+function playAudio14() {
+    let x = document.getElementById("audio14");
+    x.play();
+}
+
+document.getElementById("15").addEventListener("click", playAudio15);
+function playAudio15() {
+    let x = document.getElementById("audio15");
+    x.play();
+}
+
+document.getElementById("16").addEventListener("click", playAudio16);
+function playAudio16() {
+    let x = document.getElementById("audio16");
+    x.play();
+}
